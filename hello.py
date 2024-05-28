b142883bd5cba0b81078cfce5c4979ff07c0f09c
Sleipnir222 <87761700+Sleipnir222@users.noreply.github.com>
2024-05-27 20:38:38 -0400

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,4 +1,7 @@
 # Simon Girard  27-05-24   Assignment #2
-
+"""
+This is a simple Python program that print "hello world"
+To use it run ' python hello.py
+"""
 # This will print the string "hello world"
 print ("hello world")
